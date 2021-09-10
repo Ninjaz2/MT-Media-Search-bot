589b255abebc3c9929f3e5935b4435e9511a906f
NINJAZ-TG <90107718+NINJAZ-TG@users.noreply.github.com>
2021-09-10 14:48:21 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -105,13 +105,6 @@ async def start(bot, cmd):
                     ],
                     [
                         InlineKeyboardButton("❔ HOW TO USE ME ❓", url="https://t.me/TGmoviesUpdatesChannel/2639")
-                    ],
-                    [
-                        InlineKeyboardButton("⚠️ DISCLAIMER ⚠️", text=("**⚠️Disclaimer ⚠️
-
-                        All the files in this bot are freely available on the internet or posted by somebody else.This bot is indexing files which are already uploaded on Telegram for ease of searching,We respect all the copyright laws and works in compliance with DMCA and EUCD.If anything is against law please contact us so that it can be removed asap. 
-
-                        Contact Admin😎👉 @TGmoviesAdmin_Bot**",show_text=True)
                     ]
                 ]
             )
